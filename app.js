b9064e5dece057556b28e77463880ad8ca1c30cd
Narendra Kumar <narendrasingh909@gmail.com>
2024-09-19 10:49:59 +0530

    Added Controller File

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(express.json());
 
 const userRouter = require('./routes/users');
 app.use('/users', userRouter);
+app.use('*', async (req, res)=>{
+  res.json({
+    message: "Invalid Path",
+    status: 400
+  })
+});
 
 app.listen(9000, function(){
     console.log("Server Started.. - http://localhost:9000/");
